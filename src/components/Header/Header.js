54cbeabdc9d0a1affac61d34b3f4763789459615
Jonathon7 <40530955+Jonathon7@users.noreply.github.com>
2019-03-04 09:58:13 -0600

    initial commit (#1)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import './header.css'
-import header from '../../images/headPic.png'
+import React from "react";
+import "./header.css";
+import header from "../../images/headPic.png";
 
 export default function Header() {
-
-    return (
-        <div className='headCont'>
-            <div className='headLeft'></div>
-            <img src={header} className='headerMain'/>
-            <div className='headRight'></div>
-        </div>
-    )
+  return (
+    <div className="headCont">
+      <div className="headLeft" />
+      <img src={header} alt="" className="headerMain" />
+      <div className="headRight" />
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,28 +1,39 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
-import './main.css'
-import Header from '../Header/Header'
-import LeftMain from '../LeftMain/LeftMain'
-import RightMain from '../RightMain/RightMain'
-import homepic from '../../images/homePageDCC.jpg'
-import NavBar from '../NavBar/NavBar'
+import React, { Component } from "react";
+import "./main.css";
+import homepic from "../../images/homePageDCC.jpg";
+import NavBar from "../NavBar/NavBar";
 
 export default class Main extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        return (
-            <div> 
-                <Header/>
-                <NavBar/>
-                <div className='mainDisplay'>
-                    <LeftMain/>
-                    <img src={homepic} className='homepic'/>
-                    <RightMain/>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="main-display">
+          <img src={homepic} alt="" className="homepic" />
+        </div>
+        <div className="overlay" />
+        <div className="home-text">
+          <h1>Denton Community Chorus</h1>
+          <button>About</button>
+        </div>
+        <div className="banner" />
+        <div className="main-content">
+          <div className="main-about">
+            <h1>Denton Community Chorus</h1>
+            <p>
+              Since 1970, the Denton Community Chorus has been striving to
+              present quality choral music to the Denton community. Our mission
+              is to educate the members in the art and technique of fine choral
+              music and to offer this opportunity to all adults of the Greater
+              Denton area, and to provide the community at large opportunities
+              to experience fine choral music.
+            </p>
+          </div>
+          <div className="main-sidebar">
+            <h1>Upcoming Events</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
